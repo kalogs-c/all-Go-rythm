87aba3f5e8f633a99e9372d8907216df98665ecb
Carlinhos <carloscamilocontato@gmail.com>
2023-05-11 18:26:13 -0300

    [Feat] Bubble sort doc

--- sort/bubble.go
+++ sort/bubble.go
@@ -3,6 +3,7 @@ package sort
 import "golang.org/x/exp/constraints"
 
 // Sort an array using bubble sort algorithm
+// ascending param tells the algorithm to sort ascending (true) or descending (false)
 // WARNING:	This method mutates the slice
 func Bubble[T constraints.Ordered](slice []T, ascending bool) {
 	for i := range slice {
